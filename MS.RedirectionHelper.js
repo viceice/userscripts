922ca959a4b31f1a48715f22a5d14100e76ab5da
Michael Kriese <michael.kriese@visualon.de>
2014-08-19 08:56:36 +0200

    Updated ms redirection helper.

--- MS.RedirectionHelper.js
+++ MS.RedirectionHelper.js
@@ -1,16 +1,25 @@
+(function () {
+    "use strict";
+    
 // ==UserScript==
-// @name       	MS Redirection Helper
-// @author		Michael Kriese
-// @namespace  	http://www.visualon.de/ms
-// @version    	0.2
-// @description	MS Redirection Helper
-// @match    	http://msdn.microsoft.com/*
-// @match    	http://technet.microsoft.com/*
-// @copyright  	2014 VisualOn GmbH
-// @run-at 		document-start
+// @name        MS Redirection Helper
+// @author      Michael Kriese
+// @namespace   https://github.com/ViceIce/UserScripts
+// @version     0.3
+// @description MS Redirection Helper
+// @copyright   2014 VisualOn GmbH
+// @run-at      document-start
+//
+// @updateURL   https://raw.githubusercontent.com/ViceIce/UserScripts/master/MS.RedirectionHelper.js
+// @installURL  https://raw.githubusercontent.com/ViceIce/UserScripts/master/MS.RedirectionHelper.js
+// @downloadURL https://raw.githubusercontent.com/ViceIce/UserScripts/master/MS.RedirectionHelper.js
+//
+// @include     http://msdn.microsoft.com/*
+// @include     https://msdn.microsoft.com/*
+// @include     http://technet.microsoft.com/*
+// @include     https://technet.microsoft.com/*
 // ==/UserScript==
 
-(function () {
     var path = location.pathname;
     
     if (path.match(/^\/\w{2}-\w{2}\/library\//gi) && ! path.match(/^\/en-us\/library\//gi)){
